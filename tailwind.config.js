c18d862f6bec23b51492b903f76cc932d44b0722
Henry Bley-Vroman <olets@olets.dev>
2022-01-30 21:01:19 -1000

    feat(homepage): layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   content: ['./src/views/**/*.html.twig'],
   theme: {
     extend: {
+      backgroundImage: {
+        ocean: "url(/ocean.jpg)",
+        sky: "url(/sky.jpg)",
+      },
       link: colors.blue,
     },
   },
